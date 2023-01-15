34dc52b20fdf4694f28486e986d6afa8ab6c5d58
Vivek Pratap <vivekpratapvp11@gmail.com>
2023-01-16 00:43:56 +0530

    Prepared app for deployment

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,31 +1,3 @@
-// import axios from 'axios';
-
-// import { showAlert } from './alerts';
-
-// // const stripe = Stripe(
-// //   pk_test_51MQ6wPSBoyG2L04QM5EeFQE0YS9bPaygA61L0MqJqDNLFLYliNWWbSs6K921O8eXIct5VKSPQl7ovOcWoV6P4reL00L8OdFh9X
-// // );
-// const stripe = require('stripe')(
-//   'sk_test_51MQ6wPSBoyG2L04Q9tl24g2riu475lola1qS98RU4e4k0GQ1X5fDDXOBE3Mnvd2WM8ZeBIIgnvBmJOtYnVErywgK00RXFES0DU'
-// );
-
-// export const bookTour = async (tourId) => {
-//   try {
-//     // Get checkout session from API
-//     const session = await axios(
-//       `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
-//     );
-//     console.log(session);
-//     // Create checkout form + charge credit card
-//     await stripe.redirectToCheckout({
-//       sessionId: session.data.session.id,
-//     });
-//   } catch (err) {
-//     console.log(err);
-//     showAlert('error', err);
-//   }
-// };
-
 import axios from 'axios';
 import { showAlert } from './alerts';
 const stripe = Stripe(
@@ -35,9 +7,7 @@ const stripe = Stripe(
 export const bookTour = async (tourId) => {
   try {
     // 1) Get checkout session from API
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
     console.log(session);
 
     // 2) Create checkout form + chanre credit card
